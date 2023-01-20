999eaeb5e84da8f51214b636f81e7616e3744f62
Fathoni Zikri Nugroho <fathonizikri11@gmail.com>
2023-01-20 20:51:39 +0700

    finised ch6

--- chapter-6/books/src/App.jsx
+++ chapter-6/books/src/App.jsx
@@ -16,7 +16,9 @@ function App() {
     getBooks();
   }, []);
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete("http://localhost:3001/books/" + id);
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
@@ -24,10 +26,14 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put("http://localhost:3001/books/" + id, {
+      title: newTitle,
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
 
       return book;
